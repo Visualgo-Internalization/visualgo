91800ebd93277f0f4ebbd18f4ae13e9a3627321f
SavinVarshney <savin.varshney@gmail.com>
2015-04-20 17:13:05 +0800

    Admin can only modify the database
    - Implemented Redirection

--- js/internalization/admin_action.js
+++ js/internalization/admin_action.js
@@ -1,21 +1,51 @@
 $(document).ready(function() {
-    $('#admin-save').click(function() {
-        for (var index = 0; index <= 10000; index++) {
-            if ($('.'+index)[0]) {
-                $.ajax({
-                    type: "POST",
-                    url: "php/server.php",
-                    data: {
-                        action: "saveNewData",
-                        id: index,
-                        content: $('.'+index).text()
-                    },
-                    success: function(data) {
-                    },
-                    async: false
-                });
+    $.ajax({
+        type: "POST",
+        url: "php/admin-contributor.php",
+        data: {
+            action: "isAdmin"
+        },
+        success: function(data) {
+            if (data == "") {
+                $("#admin-menu").hide();
+            } else {
+                $("#admin-menu").show();
             }
-        }
-        alert("done");
+        },
+        async: false
+    });
+
+    $('#admin-save').click(function() {
+        $.ajax({
+            type: "POST",
+            url: "php/admin-contributor.php",
+            data: {
+                action: "isAdmin"
+            },
+            success: function(data) {
+                if (data == "") {
+                    window.location = window.location.href;
+                } else {
+                    for (var index = 0; index <= 10000; index++) {
+                        if ($('.'+index)[0]) {
+                            $.ajax({
+                                type: "POST",
+                                url: "php/server.php",
+                                data: {
+                                    action: "saveNewData",
+                                    id: index,
+                                    content: $('.'+index).text()
+                                },
+                                success: function(data) {
+                                },
+                                async: false
+                            });
+                        }
+                    }
+                    alert("done");
+                }
+            },
+            async: false
+        }); 
     });
 })
